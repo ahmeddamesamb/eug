a8a261e26ca482211f94bfd76b566b4c6004127a
laminedor <gueye.lamine1@ugb.edu.sn>
2024-05-14 15:00:18 +0000

    composant footer dans home

--- front/home/src/app/app.component.ts
+++ front/home/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { SliderComponent } from "./layouts/slider/slider.component";
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SliderComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
