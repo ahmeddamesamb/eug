53bdff41fc737b616e66014da1be4e7b3fa6b5bd
laminedor <gueye.lamine1@ugb.edu.sn>
2024-05-28 12:02:32 +0000

    navbar et routage des pages ok

---
+++ front/pats/src/app/views/gir/gestion-formations/gestion-formations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-formations',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-formations.component.html',
+  styleUrl: './gestion-formations.component.scss'
+})
+export class GestionFormationsComponent {
+
+}
