bebb94a499a8ae6c51e4cc77f40f45c0f1af254b
Ndeye khady Aidara <aidara.ndeye-khady@ugb.edu.sn>
2024-06-02 04:48:50 +0000

    Authentification avec keycloak

--- front/home/src/app/layouts/header/header.component.ts
+++ front/home/src/app/layouts/header/header.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
+  providers:[UserService, KeycloakService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  isLoggedIn: boolean = false;
+  
+  userFirstName: string | undefined;
+  userLastName: string | undefined;
+  
+  
+
+
+
+
+  public newNotifications = [
+    { id: 0, title: 'New user registered', icon: 'cilUserFollow', color: 'success' },
+    { id: 1, title: 'User deleted', icon: 'cilUserUnfollow', color: 'danger' },
+    { id: 2, title: 'Sales report is ready', icon: 'cilChartPie', color: 'info' },
+    { id: 3, title: 'New client', icon: 'cilBasket', color: 'primary' },
+    { id: 4, title: 'Server overloaded', icon: 'cilSpeedometer', color: 'warning' }
+  ];
+  
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit() {
+    this.userService.initialize();
+    this.userService.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+      if (isLoggedIn) {
+        // Mettre à jour les variables userFirstName et userLastName
+        this.userFirstName = this.userService.userFirstName;
+        this.userLastName = this.userService.userLastName;
+      }
+    });
+  }
+
+
+
+  login() {
+    this.userService.login();
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+
 
 }
---
+++ front/home/src/app/services/user.service.ts
@@ -0,0 +1,149 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { KeycloakEventType, KeycloakService } from 'keycloak-angular';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _isLoggedIn = new BehaviorSubject<boolean>(false);
+  public isLoggedIn$: Observable<boolean> = this._isLoggedIn.asObservable();
+  public accessToken: string = "";
+  public userFirstName: string | undefined;
+  public userLastName: string | undefined;
+  public userEmail: string | undefined;
+  public username: string | undefined;
+
+  constructor(private keycloakService: KeycloakService,  private router: Router // Injecter le service Router
+) {
+    this.handleAuthEvents();
+  }
+
+  async initialize() {
+    try {
+      const initSuccess = await this.keycloakService.init({
+        config: {
+          url: 'http://localhost:8080/',
+          realm: 'e-ugb',
+          clientId: 'pats-client'
+        },
+        initOptions: {
+          pkceMethod: 'S256',
+          redirectUri: 'http://localhost:4200/',
+          checkLoginIframe: false,
+         // onLoad: 'login-required'
+          
+        }
+      });
+
+      if (initSuccess) {
+        console.log("Keycloak initialisé avec succès");
+        this.updateLoginStatus();
+      } else {
+        console.error('Erreur lors de l\'initialisation de Keycloak');
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'initialisation de Keycloak', error);
+    }
+  }
+
+  private handleAuthEvents() {
+    this.keycloakService.keycloakEvents$.subscribe(event => {
+      if (event.type === KeycloakEventType.OnAuthSuccess) {
+        this.updateLoginStatus();
+      } else if (event.type === KeycloakEventType.OnAuthLogout) {
+        this._isLoggedIn.next(false);
+        this.accessToken = "";
+        this.userFirstName = undefined;
+        this.userLastName = undefined;
+        this.userEmail = undefined;
+        this.username = undefined;
+      }
+    });
+  }
+
+  async login() {
+    try {
+      await this.keycloakService.login();
+    } catch (error) {
+      console.error('Erreur lors de la connexion', error);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.keycloakService.logout();
+      this._isLoggedIn.next(false);
+      this.accessToken ="";
+      this.userFirstName = undefined;
+      this.userLastName = undefined;
+      this.userEmail = undefined;
+      this.username = undefined;
+    } catch (error) {
+      console.error('Erreur lors de la déconnexion', error);
+    }
+  }
+
+  public async updateLoginStatus() {
+   
+      // Récupérer les rôles de l'utilisateur
+      const roles = await this.keycloakService.getUserRoles();
+      const hasPatRole = roles.includes('pats');
+
+      if (hasPatRole) {
+        const userInfo = await this.keycloakService.loadUserProfile();
+        console.log("Informations de l'utilisateur :", userInfo);
+
+        this.accessToken = await this.keycloakService.getToken();
+        this.saveToken(this.accessToken);
+
+
+        // Stocker les informations de l'utilisateur dans les variables appropriées
+        this.userFirstName = userInfo.firstName;
+        this.userLastName = userInfo.lastName;
+        this.userEmail = userInfo.email;
+        this.username = userInfo.username;
+
+
+        this._isLoggedIn.next(true);
+      } 
+      else {
+        console.log("L'utilisateur n'a pas le rôle 'pats'");
+        this._isLoggedIn.next(false);
+        await this.logout();
+
+        // Rediriger l'utilisateur vers la page NotAuthorizedComponent
+        const navigationExtras: NavigationExtras = {
+          queryParamsHandling: 'preserve',
+          preserveFragment: true
+        };
+        this.router.navigate(['/404'], navigationExtras);
+      
+    } 
+  
+  }
+  public getUserFirstName(): string | undefined {
+    return this.userFirstName;
+  }
+
+  // Nouvelle méthode pour récupérer le lastName
+  public getUserLastName(): string | undefined {
+    return this.userLastName;
+  }
+
+ // Méthode pour récupérer le token depuis le localStorage
+getToken(){
+  return localStorage.getItem("token");
+}
+
+// Méthode pour enregistrer le token dans le localStorage
+saveToken(token: string) {
+  localStorage.setItem("token", token);
+}
+
+
+
+
+
+}
