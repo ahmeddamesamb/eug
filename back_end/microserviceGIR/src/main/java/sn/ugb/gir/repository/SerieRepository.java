6de823b3dd4b89ffac8a32c400c23d2c6038600f
smbn <93800484+smbn@users.noreply.github.com>
2024-06-04 17:27:29 +0000

    Serie validate data

--- back_end/microserviceGIR/src/main/java/sn/ugb/gir/repository/SerieRepository.java
+++ back_end/microserviceGIR/src/main/java/sn/ugb/gir/repository/SerieRepository.java
@@ -4,9 +4,17 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import sn.ugb.gir.domain.Serie;
 
+import java.util.Optional;
+
 /**
  * Spring Data JPA repository for the Serie entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface SerieRepository extends JpaRepository<Serie, Long> {}
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByLibelleSerie(String libelleSerie);
+
+    Optional<Serie> findBySigleSerie(String sigleSerie);
+
+    Optional<Serie> findByCodeSerie(String codeSerie);
+}
