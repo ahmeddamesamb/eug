13d54292fad290393cff73a411cb8391df2668c1
Ndeye khady Aidara <aidara.ndeye-khady@ugb.edu.sn>
2024-06-06 13:04:17 +0000

    Modification constantes.globales.ts

--- exemple/environment-pats.ts
+++ exemple/environment-pats.ts
@@ -5,7 +5,7 @@ export const ENVIRONMENT = {
     UrlEspacePats : 'http://localhost:4201',
     UrlEspacePer : 'http://localhost:4202',
     UrlEspaceEtudiant : 'http://localhost:4203',
-    apiURL: 'http://localhost:8081/services/microservicegir/api'
+    endpointURL: 'http://localhost:8081/services/'
 
 }
 
