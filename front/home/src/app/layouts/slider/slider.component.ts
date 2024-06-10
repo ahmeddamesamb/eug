4bff9b5ea4426a04b35ed1d07ecd7b35ba2c1dc2
laminedor <gueye.lamine1@ugb.edu.sn>
2024-06-10 11:50:32 +0000

    correction lien home et boutton annuler

--- front/home/src/app/layouts/slider/slider.component.ts
+++ front/home/src/app/layouts/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 
 @Component({
@@ -9,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './slider.component.scss'
 })
 export class SliderComponent {
+
+  public urlLogin = environment.urlLogin; 
+  public realm_pats = environment.realm_pats;
+  public realm_per = environment.realm_per;
+  public realm_etudiant = environment.realm_etudiant; 
+  public redirectUrl_pats = environment.redirectUrl_pats;
+  public redirectUrl_per = environment.redirectUrl_per;
+  public redirectUrl_etudiant = environment.redirectUrl_etudiant;
+  public clientId = environment.clientId;
+  public suiteUrlLogin = environment.suiteUrlLogin;
+
+  public getUrl(realm:string, client:string): string{
+    return  `${this.urlLogin}${realm}${this.suiteUrlLogin}${this.clientId}`;
+  }
 
 }
