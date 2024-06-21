65b3765db177f5d2287744a1d81b4241d69b2d16
laminedor <gueye.lamine1@ugb.edu.sn>
2024-06-21 12:43:59 +0000

    service spécialite ok

--- front/pats/src/app/views/gir/parametrage/components/specialite/models/specialite-model.ts
+++ front/pats/src/app/views/gir/parametrage/components/specialite/models/specialite-model.ts
@@ -1,2 +1,9 @@
+import {MentionModel} from '../../mention/models/mention-model';
 export interface SpecialiteModel {
+    id?:number,
+    nomSpecialites?:string,
+    sigleSpecialites?:string,
+    specialiteParticulierYN?:boolean,
+    specialitesPayanteYN?:boolean,
+    mention?: MentionModel,
 }
