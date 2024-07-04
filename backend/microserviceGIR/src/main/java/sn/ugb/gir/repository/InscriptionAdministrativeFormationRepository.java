7ac764de6f5b7e7e6c7946f0c570f69c641509a1
Rose Kuntz <diouf.mame-awa-bousso@ugb.edu.sn>
2024-07-04 12:32:49 +0000

    Choisir une formation OK

--- backend/microserviceGIR/src/main/java/sn/ugb/gir/repository/InscriptionAdministrativeFormationRepository.java
+++ backend/microserviceGIR/src/main/java/sn/ugb/gir/repository/InscriptionAdministrativeFormationRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import sn.ugb.gir.domain.InscriptionAdministrativeFormation;
 
+import java.util.Optional;
+
 /**
  * Spring Data JPA repository for the InscriptionAdministrativeFormation entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface InscriptionAdministrativeFormationRepository extends JpaRepository<InscriptionAdministrativeFormation, Long> {}
+public interface InscriptionAdministrativeFormationRepository extends JpaRepository<InscriptionAdministrativeFormation, Long> {
+    Optional<InscriptionAdministrativeFormation>  findByFormationIdAndInscriptionAdministrativeEtudiantIdAndInscriptionAdministrativeAnneeAcademiqueId(Long idFormation, Long idEtudiant,Long idAnneeAcademique);
+    Optional<InscriptionAdministrativeFormation>  findByInscriptionAdministrativeEtudiantIdAndInscriptionAdministrativeAnneeAcademiqueId(Long idEtudiant,Long idAnneeAcademique);
+}
