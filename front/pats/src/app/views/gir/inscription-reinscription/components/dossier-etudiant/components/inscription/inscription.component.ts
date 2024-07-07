1fc1783be8bb5fd27976ee693086a3f2a6681186
Ndeye khady Aidara <aidara.ndeye-khady@ugb.edu.sn>
2024-07-07 17:05:31 +0000

    Choisir formation

--- front/pats/src/app/views/gir/inscription-reinscription/components/dossier-etudiant/components/inscription/inscription.component.ts
+++ front/pats/src/app/views/gir/inscription-reinscription/components/dossier-etudiant/components/inscription/inscription.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { AnneeAcademiqueService } from '../../../../../parametrage/components/annee-academique/services/annee-academique.service';
-import { FormationService } from 'src/app/views/gir/parametrage/components/formation/services/formation.service';
-import { AlertServiceService } from 'src/app/shared/services/alert/alert-service.service';
 import { RowComponent, ColComponent, TextColorDirective, CardComponent, DatePickerComponent as DatePickerComponent_1, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, FormFeedbackComponent, InputGroupComponent, InputGroupTextDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ListGroupDirective, ListGroupItemDirective } from '@coreui/angular-pro';
 import { DocsExampleComponent } from '@docs-components/public-api';
 import { DatePipe } from '@angular/common';
+import {NiveauService} from '../../../../../parametrage/components/niveau/services/niveau.service';
+import { NiveauModel } from 'src/app/views/gir/parametrage/components/niveau/models/niveau-model';
+import { UfrServiceService } from 'src/app/views/gir/parametrage/components/ufr/services/ufr-service.service';
+import { UfrModel } from 'src/app/views/gir/parametrage/components/ufr/models/ufr-model';
 
 @Component({
   selector: 'app-inscription',
@@ -22,14 +23,17 @@ export class InscriptionComponent {
   customStylesValidated = false;
   id: number | undefined ;
   inscriptionForm: FormGroup;
+  niveaux: NiveauModel[] = [];
+  ufrs : UfrModel[] = [];
 
  
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private alertService: AlertServiceService,
-    private formationService: FormationService
+    private niveauService: NiveauService,
+    private ufrService: UfrServiceService,
+  
   ) {
     this.inscriptionForm = new FormGroup({
       UFR: new FormControl('', Validators.required),
@@ -40,9 +44,34 @@ export class InscriptionComponent {
   }
 
   ngOnInit() {
+    this.loadNiveaux();
+    this.loadUfrs();
    
   }
 
+
+  loadNiveaux() {
+    this.niveauService.getNiveauList()
+    .subscribe({
+      next: (data) => {
+        this.niveaux = data;
+      },
+      error: (err) => {
+        console.error("Erreur lors du chargement des niveaux:", err);
+      }
+    });
+  }
+    loadUfrs(){
+      this.ufrService.getUfrList()
+      .subscribe({
+        next: (data) => {
+          this.ufrs = data;
+        },
+        error: (err) => {
+          console.error("Erreur lors du chargement des niveaux:", err);
+        }
+      });
+  }
 
 
 
